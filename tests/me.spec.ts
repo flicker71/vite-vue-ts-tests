4a1c03edaafd8284800a94664a3a081e21aa8dae
Alloula Elias <72727849+flicker71@users.noreply.github.com>
2024-01-18 16:54:21 +0100

    premier tests

---
+++ tests/me.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test('Bouton candidature', async ({ page }) => {
+    await page.goto('https://www.mydigitalschool.com/ecole-multimedia-lyon?gclid=Cj0KCQiAtaOtBhCwARIsAN_x-3KhAJ0P2I-QtbLniSdc1DCm-TI_a626kgjJBda3R5JntsjGWBp5acsaAsyGEALw_wcB');
+    await page.getByRole('button', { name: 'Avec plaisir' }).click();
+
+  await expect(page.locator('#navbarResponsive').getByRole('link', { name: 'Candidature' })).toBeVisible();
+});
+
+test('candidature', async ({ page }) => {
+    await page.goto('https://www.mydigitalschool.com/ecole-multimedia-lyon?gclid=Cj0KCQiAtaOtBhCwARIsAN_x-3KhAJ0P2I-QtbLniSdc1DCm-TI_a626kgjJBda3R5JntsjGWBp5acsaAsyGEALw_wcB');
+    await page.getByRole('button', { name: 'Avec plaisir' }).click();
+    await page.locator('#navbarResponsive').getByRole('link', { name: 'Candidature' }).click();
+    await page.getByLabel('École envisagée').selectOption('464');
+    await page.getByLabel('Formation envisagée', { exact: true }).selectOption('38106');
+    await page.getByPlaceholder('Prénom *').click();
+    await page.getByPlaceholder('Prénom *').fill('John');
+    await page.getByPlaceholder('Prénom *').press('Tab');
+    await page.getByPlaceholder('Nom *', { exact: true }).fill('Due');
+    await page.getByPlaceholder('Email *').click();
+    await page.getByPlaceholder('Email *').fill('johndue@gmail.com');
+    await page.getByPlaceholder('Téléphone *').click();
+    await page.getByPlaceholder('Téléphone *').fill('+33666666666');
+    await page.getByLabel('Niveau d\'études').selectOption('517');
+    await page.getByLabel('Pour valider le formulaire,').check();
+    await page.getByRole('button', { name: 'Télécharger' }).click();
+    await expect(page.getByRole('heading', { name: 'Demande de candidature' })).toBeVisible();
+});
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
